08f17f18c6816a9029e3c71db9a6333e6dd0be7c
lordamotar <lordamotar@gmail.com>
2026-04-12 18:11:25 +0500

    fix: allow all CORS origins for production

--- app/main.py
+++ app/main.py
@@ -55,7 +55,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://192.168.1.104:3000"],
+    allow_origins=["*"],  # Разрешаем все источники для продакшена
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
